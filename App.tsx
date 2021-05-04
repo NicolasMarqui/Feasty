7cae2d9326d81d20c1e071cf95feed4995323fde
Nicolas <nicolas.marqui@fatec.sp.gov.br>
2021-05-04 17:44:05 -0300

    dev: Added react native paper and styled components, added theme configuration

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/theme";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </ThemeProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/theme/fonts.ts
@@ -0,0 +1,18 @@
+export const fonts = {
+    body: "NunitoSans_400Regular",
+    bold: "NunitoSans_700Bold",
+    extrabold: "NunitoSans_800ExtraBold",
+};
+
+export const fontWeights = {
+    regular: 400,
+    bold: 700,
+    extraBold: 800,
+};
+
+export const fontSizes = {
+    regularTitle: "25px",
+    cardTitle: "20px",
+    xs: "12px",
+    sm: "20px",
+};
