41aec844ccc1ba2d71a44b2e7d68f2bad892f074
Nicolas <nicolas.marqui@fatec.sp.gov.br>
2021-05-08 15:24:40 -0300

    dev: Saving favorites inside async storage

--- src/services/favorites/favorites.context.tsx
+++ src/services/favorites/favorites.context.tsx
@@ -1,5 +1,6 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { RestaurantMockProps } from "../../utils/types";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface FavoritesContextProps {
     favorites?: RestaurantMockProps[] | [];
@@ -19,6 +20,26 @@ export const FavoritesContextProvider: React.FC<FavoritesContextProviderProps> =
 }) => {
     const [favorites, setFavorites] = useState<RestaurantMockProps[]>([]);
 
+    const saveFavorites = async (value: any) => {
+        try {
+            const jsonValue = JSON.stringify(value);
+            await AsyncStorage.setItem("@feasty:favorites", jsonValue);
+        } catch (e) {
+            console.log("Save favorites error");
+        }
+    };
+
+    const loadFavorites = async () => {
+        try {
+            const value = await AsyncStorage.getItem("@feasty:favorites");
+            if (value !== null) {
+                setFavorites(JSON.parse(value));
+            }
+        } catch (e) {
+            console.log("Load favorites error");
+        }
+    };
+
     const add = (restaurant: RestaurantMockProps) => {
         setFavorites([...favorites, restaurant]);
     };
@@ -29,6 +50,14 @@ export const FavoritesContextProvider: React.FC<FavoritesContextProviderProps> =
         );
         setFavorites(newFavorites);
     };
+
+    useEffect(() => {
+        loadFavorites();
+    }, []);
+
+    useEffect(() => {
+        saveFavorites(favorites);
+    }, [favorites]);
 
     return (
         <FavoritesContext.Provider
